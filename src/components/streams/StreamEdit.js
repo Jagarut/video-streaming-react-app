637a72eb4965a942d3fda0faae72ef10f71d2283
Jag <jesusafonso.eam@gmail.com>
2020-08-25 16:02:40 +0100

    stream components finished

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,46 @@
 import React from "react";
+import _ from "lodash";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./streamForm";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.id);
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.id, formValues);
+  };
+
+  renderStream() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+  render() {
+    console.log(this.props.stream);
+    return <div>{this.renderStream()}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const { id } = ownProps.match.params;
+  return {
+    stream: state.streams[id],
+    id: id, // id to be send to fetchtStream & editStream as argument
+  };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,77 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId)
+      return (
+        <div className="right floated content">
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link
+            to={`/streams/delete/${stream.id}`}
+            className="ui button negative"
+          >
+            Delete
+          </Link>
+        </div>
+      );
+  }
+
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <div>{this.renderAdmin(stream)}</div>
+          <i className="large middle aligned icon camera" />
+
+          <div className="content">
+            <Link to={`/streams/show/${stream.id}`} className="header">
+              {stream.title}
+            </Link>
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  renderCreate() {
+    if (this.props.userIsSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    userIsSignedIn: state.auth.isSignedIn,
+  }; //turning object into array
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
